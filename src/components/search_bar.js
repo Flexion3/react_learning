f992a150b9222a6fc0a631a84be69d8d1332e85b
JonasCrauwels <crauwels.93@gmail.com>
2017-06-04 11:35:10 +0200

    searchbar ready

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,25 @@
 /**
  * Created by Jonas on 31/05/2017.
  */
-import React from 'react'
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input/>;
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {term:''};
+    }
+
+    render (){
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value})}/>
+                <p>state: {this.state.term}</p>
+            </div>
+        );
+    }
 }
 
 export default SearchBar;
